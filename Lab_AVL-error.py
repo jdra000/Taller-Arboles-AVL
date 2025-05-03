8d866fdc31b2e12c42d517c5af995744870d5e57
jdra000 <juandavidrey@icloud.com>
2025-05-02 21:56:04 -0500

    all

--- Lab_AVL-error.py
+++ Lab_AVL-error.py
@@ -63,25 +63,85 @@ class AVLTree:
         elif value > node.value:
             node.right = self._insert_recursive(node.right, value)
         else:
-            return node 
-        
+            return node  # Duplicate values not allowed
+
         updateHeight(node)
-        
         balance = getBalance(node)
         node.balance = balance
 
-        #if balance > 1 and getBalance(node.left) >= 0:
-            #rotate_right(node) 
-        #elif balance > 1 and getBalance(node.left) < 0:
-            #node.left = rotate_left(node.left)
-            #rotate_right(node) 
-        if balance > 1 and getBalance(node.right) > 0:
-            rotate_left(node)
-        #elif balance < -1 and getBalance(node.right) > 0:
-            #node.right = rotate_right(node.right)
-            #rotate_left(node) 
-        
-        return node 
+        # Right Heavy (balance > 1)
+        if balance > 1:
+            # Right-Right Case
+            if getBalance(node.right) > 0:
+                return rotate_left(node)
+            # Right-Left Case
+            elif getBalance(node.right) < 0:
+                node.right = rotate_right(node.right)
+                return rotate_left(node)
+
+        # Left Heavy (balance < -1)
+        if balance < -1:
+            # Left-Left Case
+            if getBalance(node.left) < 0:
+                return rotate_right(node)
+            # Left-Right Case
+            elif getBalance(node.left) > 0:
+                node.left = rotate_left(node.left)
+                return rotate_right(node)
+
+        return node
+    
+    def delete(self, value):
+        self.root = self._delete_recursive(self.root, value)
+
+    def _delete_recursive(self, node, value):
+        if not node:
+            return node
+
+        if value < node.value:
+            node.left = self._delete_recursive(node.left, value)
+        elif value > node.value:
+            node.right = self._delete_recursive(node.right, value)
+        else:
+            # Nodo con solo un hijo o sin hijos
+            if not node.left:
+                return node.right
+            elif not node.right:
+                return node.left
+            
+            # Nodo con dos hijos: obtener el sucesor in-order (mínimo del subárbol derecho)
+            min_larger_node = self._get_min_value_node(node.right)
+            node.value = min_larger_node.value
+            node.right = self._delete_recursive(node.right, min_larger_node.value)
+
+        updateHeight(node)
+        balance = getBalance(node)
+        node.balance = balance
+
+        # Rebalanceo tras eliminación
+        if balance < -1:
+            if getBalance(node.left) <= 0:
+                return rotate_right(node)
+            else:
+                node.left = rotate_left(node.left)
+                return rotate_right(node)
+
+        if balance > 1:
+            if getBalance(node.right) >= 0:
+                return rotate_left(node)
+            else:
+                node.right = rotate_right(node.right)
+                return rotate_left(node)
+
+        return node
+
+    def _get_min_value_node(self, node):
+        current = node
+        while current.left:
+            current = current.left
+        return current
+
+
     
 
 def level_order_rec(root, level, res):
@@ -110,9 +170,15 @@ print("Insertando valores:", values_to_insert)
 for val in values_to_insert:
     avl.insert(val)
 
+
+print("\n--- Después de inserciones ---")
+
 res = level_order(avl.root)
 for level in res:
     print(f'[{", ".join(map(str, level))}] ', end='')
 
-print("\n--- Después de inserciones ---")
+avl.delete(10)
 
+res = level_order(avl.root)
+for level in res:
+    print(f'[{", ".join(map(str, level))}] ', end='')
